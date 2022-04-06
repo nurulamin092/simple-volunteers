ce45626b43a98bbce1af5729df4b6e27da157e3c
nurulamin092 <mnurulamin092@gmial.com>
2022-04-06 11:00:27 +0600

    routers added

---
+++ src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Home.css'
+const Home = () => {
+    return (
+        <div>
+            <button>Click</button>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/Volunteers/Volunteers.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Volunteers.css'
+const Volunteers = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Volunteers;
